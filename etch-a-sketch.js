3fea59d7fd243239dab685f7217efa87dfc2ff78
Sarthak Garg <sarthakgarg97@hotmail.com>
2021-09-22 00:46:09 +1000

    Add JS event listeners to change background colour of grid element on mouseover

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,11 +4,16 @@ function addElement() {
     //const newContent = document.createTextNode("Hi there and greetings!");
     //newDiv.appendChild(newContent);
     document.getElementById("grid-container").appendChild(newDiv);
+    newDiv.addEventListener("mouseover", () => {
+        newDiv.style.backgroundColor = "white";
+    });
 }
+
 
 function addElementLoop(){
     for (let i=1; i<=256; i++){
         addElement();
+
     }
 
     //setting grid property by JS so it can be changed later
