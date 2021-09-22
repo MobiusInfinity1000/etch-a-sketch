023afced5a5fc87afabd352524ca2f4d3557e90a
Sarthak Garg <sarthakgarg97@hotmail.com>
2021-09-22 23:24:59 +1000

    Adding dynamic change grid size button, clean up css, add slider, fix background

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,32 +1,64 @@
+//get HTML elements
+const slider = document.getElementById("slider");
+const output = document.getElementById("sliderVal");
+const gridContainer = document.getElementById("grid-container");
+const changeGridButton = document.getElementById("changeGridButton");
+const clearButton = document.getElementById('clearButton');
+let gridSize = parseInt(slider.value,10);
+
+
+//update slider value on load
+output.innerHTML = slider.value;
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+  }
+
+
 function addElement() {
     const newDiv = document.createElement("div");
     newDiv.className = "grid-element";
     //const newContent = document.createTextNode("Hi there and greetings!");
     //newDiv.appendChild(newContent);
-    document.getElementById("grid-container").appendChild(newDiv);
+    gridContainer.appendChild(newDiv);
     newDiv.addEventListener("mouseover", () => {
-        newDiv.style.backgroundColor = "white";
+        newDiv.style.backgroundColor = "black";
     });
 }
 
+function changeGridSize(){
+    gridContainer.innerHTML = '';
+    let gridSize = parseInt(slider.value,10);
+    console.log(gridSize*gridSize);
+    console.log(typeof Math.pow(gridSize,2));
+    for (let i=1; i<=Math.pow(gridSize,2); i++){
+        addElement();
+
+    };
+      gridContainer.style["grid-template-columns"] = "repeat(" + gridSize + ", 1fr)";
+}
 
 function addElementLoop(){
     for (let i=1; i<=256; i++){
         addElement();
-
     }
 
     //setting grid property by JS so it can be changed later
-    document.getElementById("grid-container").style["grid-template-columns"] = "repeat(16, 1fr)";
+    gridContainer.style["grid-template-columns"] = "repeat(16, 1fr)";
 }
+
+//When document loads, creates default value of 16 by 16 boxes
 document.body.onload = addElementLoop;
 
+//EVENT LISTENERS
 //clear button functionality
-const clearButton = document.getElementById('clearButton');
 clearButton.addEventListener("click", () => {
     const gridDivs = document.getElementById("grid-container").querySelectorAll(".grid-element");
-    for (let i=0; i<256; i++){
-        gridDivs[i].style.backgroundColor = "#062F4F"; //assign colour to white later on 
+    for (let i=0; i<=gridDivs.length; i++){
+        gridDivs[i].style.backgroundColor = "transparent"; //assign colour to white later on 
     }
 });
+//change grid button functionality
+changeGridButton.addEventListener("click", changeGridSize);
 
