03b141d952275d4a0decef0104fa8ef479c3d1bb
Sarthak Garg <sarthakgarg97@hotmail.com>
2021-09-24 00:47:54 +1000

    Fixed console log error for Clear Button, addedcolour picker and corresponding js logic

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,7 +4,11 @@ const output = document.getElementById("sliderVal");
 const gridContainer = document.getElementById("grid-container");
 const changeGridButton = document.getElementById("changeGridButton");
 const clearButton = document.getElementById('clearButton');
+const rgbButton = document.getElementById('rainbowButton');
+// const blackButton = document.getElementById('blackButton');
+const customcolourButton = document.getElementById('customColour');
 let gridSize = parseInt(slider.value,10);
+let rgbFlag = 0;
 
 
 //update slider value on load
@@ -13,17 +17,6 @@ output.innerHTML = "Grid Width: " + slider.value;
 // Update the current slider value (each time you drag the slider handle)
 
 
-
-function addElement() {
-    const newDiv = document.createElement("div");
-    newDiv.className = "grid-element";
-    //const newContent = document.createTextNode("Hi there and greetings!");
-    //newDiv.appendChild(newContent);
-    gridContainer.appendChild(newDiv);
-    newDiv.addEventListener("mouseover", () => {
-        newDiv.style.backgroundColor = generateRandomColour();
-    });
-}
 
 function changeGridSize(){
     gridContainer.innerHTML = ''; //clear grid Container before adding new divs
@@ -39,20 +32,55 @@ function addElementLoop(){
     for (let i=1; i<=256; i++){
         addElement();
     }
-
     //setting grid property by JS so it can be changed later
     gridContainer.style["grid-template-columns"] = "repeat(16, 1fr)";
-}
+};
 
 function generateRandomInt() {
     let randomnumber = Math.floor(Math.random() * (256 - 0 + 1)) + 0; //as max rgb value is 255
     return randomnumber;
-}
+};
 
 function generateRandomColour(){
     rgbCol = "rgb(" + generateRandomInt() + "," + generateRandomInt() + "," + generateRandomInt() + ")";
     return rgbCol;
-}
+};
+
+function fillGridElement() {
+    if (rgbFlag==1){
+        this.style.backgroundColor = generateRandomColour();
+    } else {
+        this.style.backgroundColor = customcolourButton.value;
+    };
+};
+
+function addElement() {
+    const newDiv = document.createElement("div");
+    newDiv.className = "grid-element";
+    //const newContent = document.createTextNode("Hi there and greetings!");
+    //newDiv.appendChild(newContent);
+    gridContainer.appendChild(newDiv);
+    newDiv.addEventListener("mouseover", fillGridElement);
+};
+
+
+// function addElement() {
+//     const newDiv = document.createElement("div");
+//     newDiv.className = "grid-element";
+//     //const newContent = document.createTextNode("Hi there and greetings!");
+//     //newDiv.appendChild(newContent);
+//     gridContainer.appendChild(newDiv);
+//     newDiv.addEventListener("mouseover", () => {
+//         newDiv.style.backgroundColor = generateRandomColour();
+//     });
+// };
+
+function drawRandomColour() {
+    const gridDivs = document.getElementById("grid-container").querySelectorAll(".grid-element");
+    for (let i=0; i<=gridDivs.length; i++){
+        gridDivs[i].removeEventListener()
+    }
+};
 
 //When document loads, creates default value of 16 by 16 boxes
 document.body.onload = addElementLoop;
@@ -66,10 +94,24 @@ slider.oninput = function() {
 //clear button functionality
 clearButton.addEventListener("click", () => {
     const gridDivs = document.getElementById("grid-container").querySelectorAll(".grid-element");
-    for (let i=0; i<=gridDivs.length; i++){
-        gridDivs[i].style.backgroundColor = "transparent";
-    }
+    for (let i=0; i<gridDivs.length; i++){
+        gridDivs[i].style.backgroundColor = "white";
+    };
+    
 });
+
+rgbButton.addEventListener("click", () => {
+    rgbFlag = 1;
+});
+
+// blackButton.addEventListener("click", () => {
+//     rgbFlag = 0;
+// })
+
+customcolourButton.addEventListener("click", () => {
+    rgbFlag = 0;
+})
+
 //change grid button functionality
 changeGridButton.addEventListener("click", changeGridSize);
 
